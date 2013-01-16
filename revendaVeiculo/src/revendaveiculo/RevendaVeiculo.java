457128613face27ecd07f0270458b0a8d526fc3f
Fernando <Fernando@Fernando-PC>
2013-01-16 12:11:31 -0200

    - Testando repositorio!

---
+++ revendaVeiculo/src/revendaveiculo/RevendaVeiculo.java
@@ -0,0 +1,13 @@
+package revendaveiculo;
+
+/**
+ *
+ * @author Fernando, Gustavo 
+ */
+public class RevendaVeiculo {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
